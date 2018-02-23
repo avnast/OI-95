65a7fb5861430019d6a90247b23404e2b772b724
avnast <av.nast@gmail.com>
2018-02-23 10:44:35 +0200

    added oct

--- src/app/main.py
+++ src/app/main.py
@@ -7,13 +7,15 @@ def index():
     d = request.values.get('d', '')
     msg = ''
     h = ''
+    o = ''
     b = ''
     if d != '':
         try:
             h = hex(long(d))
+            o = oct(long(d))
             b = bin(long(d))
         except:
             msg = 'Please enter valid decimal number'
             
-    return render_template('index.html', d=d, h=h, b=b, msg=msg)
+    return render_template('index.html', d=d, h=h, o=o, b=b, msg=msg)
 
